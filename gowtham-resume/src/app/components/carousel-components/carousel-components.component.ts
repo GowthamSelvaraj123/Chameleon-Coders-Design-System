99bf47f82a1a04c2a0e6b9fc905d7b9968246d1b
GowthamSelvaraj123 <gowthamdesigner94@gmail.com>
2024-07-30 22:37:21 +0530

    Carousel Component

--- gowtham-resume/src/app/components/carousel-components/carousel-components.component.ts
+++ gowtham-resume/src/app/components/carousel-components/carousel-components.component.ts
@@ -5,10 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './carousel-components.component.html',
   styleUrl: './carousel-components.component.scss'
 })
+
 export class CarouselComponentsComponent {
     heroContents = [
       {title:"I am Front End Developer", content:"I have versatile skills in angular, react js, html ,css", button:"Contact Us"}, 
       {title:"I am PHP Developer", content:"I have versatile skills in php and mysql projects", button:"Contact us"}, 
       {title:"I am Full Stack Developer", content:"I have well at wordpress, html, css, js, jquery, php, mysql", button:"Contact us"}, 
     ];
+    activeIndex = 0; 
+    totalSlides:number = this.heroContents.length;
+    setActiveIndex(index: number) {
+      this.activeIndex = index;
+    }
+    onNext()
+    {
+      if(this.activeIndex < this.totalSlides - 1){
+      this.activeIndex++;
+      }
+      else
+      {
+        this.activeIndex = 0;
+      }
+      console.log(this.activeIndex);
+    }
+    onPrev()
+    {
+      if (this.activeIndex > 0) {
+      this.activeIndex--;
+      }
+      else {
+        this.activeIndex = this.totalSlides - 1;
+      }
+      console.log(this.activeIndex);
+    }
 }
