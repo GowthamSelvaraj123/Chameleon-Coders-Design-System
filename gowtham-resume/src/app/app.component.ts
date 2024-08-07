ac685045f70a8cdf3cbbb1fd41357da94451666f
GowthamSelvaraj123 <gowthamdesigner94@gmail.com>
2024-08-07 07:22:08 +0530

    Slider functionality completed

--- gowtham-resume/src/app/app.component.ts
+++ gowtham-resume/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
 import { AppModuleModule } from './app.module';
+import { FooterComponentComponent } from './components/footer-component/footer-component.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports:[RouterOutlet, HeaderComponentComponent, AppModuleModule],
+  imports:[RouterOutlet, HeaderComponentComponent, AppModuleModule, FooterComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
