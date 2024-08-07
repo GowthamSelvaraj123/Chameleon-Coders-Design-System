c27ca2ac3d97ec8742b020708494705267b8f262
GowthamSelvaraj123 <gowthamdesigner94@gmail.com>
2024-08-07 07:16:59 +0530

    Button update and slider component update

--- gowtham-resume/src/app/components/carousel-components/carousel-components.component.ts
+++ gowtham-resume/src/app/components/carousel-components/carousel-components.component.ts
@@ -38,4 +38,8 @@ export class CarouselComponentsComponent {
       }
       console.log(this.activeIndex);
     }
+    matchDot(index: number): void
+    {
+      this.activeIndex = index;
+    }
 }
